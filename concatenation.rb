0fdf8bb56dfb1f3ee56d073c6d6cac6f99f4803b
rootDEV2990 <website.dev@icloud.com>
2020-04-27 11:10:51 -0500

    Showcase of how to concat in ruby lang

---
+++ concatenation.rb
@@ -0,0 +1,6 @@
+# Concat with poerator "+" plus operator
+p "join these strings " + "with plus operator"
+# Concat with poerator "<<" shovel operator
+p "join these strings " + "with shovel operator"
+# Concat with ".concat" methodshovel operator
+p "join these strings ".concat("with concat method")
