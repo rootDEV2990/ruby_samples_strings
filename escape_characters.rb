e082be98d3ddc423c297bc6f260a17960e50fe81
rootDEV2990 <website.dev@icloud.com>
2020-04-27 11:13:35 -0500

    Added examples of available escape chars in ruby

---
+++ escape_characters.rb
@@ -0,0 +1,8 @@
+#escape characters 
+
+p "this is \\ backslask"\\  #=> Need a backslash in your string?
+p "this is \b backspace"\b  #=> Backspace
+p "this is \r carrige return"\r  #=> Carriage return, for those of you that love typewriters
+p "this is \n newline"\n  #=> Newline. You'll likely use this one the most.
+p "this is \s space"\s  #=> Space
+p "this is \t tab"\t  #=> Tab
